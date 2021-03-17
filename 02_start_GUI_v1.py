6f4ebdcac892876772536143e4cf7a8cc7e193c2
kimmy2512 <51727300+kimmy2512@users.noreply.github.com>
2021-03-17 13:20:25 +1300

    02 continue

--- 02_start_GUI_v1.py
+++ 02_start_GUI_v1.py
@@ -5,6 +5,7 @@ import random
 
 class Start:
     def __init__(self, parent):
+
         # GUI to get starting balance and stakes
         self.start_frame = Frame(padx=10, pady=10)
         self.start_frame.grid()
@@ -12,20 +13,80 @@ class Start:
         # Mystery Heading (row 0)
         self.mystery_box_label = Label(self.start_frame, text="Mystery Box Game",
                                        font="Arial 19 bold")
-        self.mystery_box_label.grid(row=1)
+        self.mystery_box_label.grid(row=0)
+
+        # Initial instructions (row 1)
+        self.mystery_instructions = Label(self.start_frame, font="Arial 10 italic",
+                                          text="Please enter a dollar amount "
+                                                "(between $5 and $50) in the box "
+                                                "below. Then choose the stakes. "
+                                                "The higher the stakes, the more "
+                                                "you can win!",
+                                          wrap=275, justify=LEFT, padx=10, pady=10)
+        self.mystery_instructions.grid(row=1)
 
         # Entry box... (row 1)
-        self.start_amount_entry = Entry(self.start_frame, font="Arial 16 bold")
+        self.start_amount_entry = Entry(self.start_frame, font="Arial 19 bold")
         self.start_amount_entry.grid(row=2)
 
-        # Play Button (row 2)
-        self.lowstakes_button = Button(text="Low ($5)",
-                                       command=lambda: self.to_game(1))
-        self.lowstakes_button.grid(row=2, pady=10)
+        # Button frame (row 3)
+        self.stakes_frame = Frame(self.start_frame)
+        self.stakes_frame.grid(row=3)
+
+        # Buttons go here.
+        button_font = "Arial 12 bold"
+        # Orange low stakes button...
+        self.lowstakes_button = Button(self.stakes_frame, text="Low ($5)",
+                                       command=lambda: self.to_game(1),
+                                       font=button_font, bg="#FF9933")
+        self.lowstakes_button.grid(row=0, column=0, pady=10)
+
+        # Yellow medium stakes button...
+        self.lowstakes_button = Button(self.stakes_frame, text="Medium ($10)",
+                                       command=lambda: self.to_game(2),
+                                       font=button_font, bg="#FFFF33")
+        self.lowstakes_button.grid(row=0, column=1, padx=5, pady=10)
+
+        # Green high stakes button...
+        self.lowstakes_button = Button(self.stakes_frame, text="High ($15)",
+                                       command=lambda: self.to_game(3),
+                                       font=button_font, bg="#99FF33")
+        self.lowstakes_button.grid(row=0, column=2, pady=10)
+
+        # Help Button (row 2)
+        self.help_button = Button(self.start_frame, text="How to Play",
+                                  bg="#808080", fg="white", font=button_font)
+        self.help_button.grid(row=4, pady=10)
 
     def to_game(self, stakes):
         starting_balance = self.start_amount_entry.get()
-        Game(self, stakes, starting_balance)
+
+        # Set error background colours (and assume that there are no
+        # errors at the start...
+        error_back = "#ffafaf"
+        has_errors = "no"
+
+        # Change background to white (for testing purposes)...
+        self.start_amount_entry.config(bg="white")
+        self.amount_error_label.config(text="")
+
+        try:
+            starting_balance = int(starting_balance)
+
+            if starting_balance < 5:
+                has_errors = "yes"
+                error_feedback = "Sorry, the least you " \
+                                 "can play with is $5"
+            elif starting_balance > 50:
+                has_errors = "yes"
+                error_feedback = "Too high! The most you can risk in " \
+                                 "this game is $50"
+            elif starting_balance < 10 and (stakes == 2 or stakes == 3):
+                has_errors = "yes"
+                error_feedback = "Sorry, you can only afford to " \
+                                 "play a low or medium stakes game."
+                
+
 
 
 class Game:
